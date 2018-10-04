b769db7871e3fdc4a8baca2a8b468e4d11289802
wincert <touyailn@gmail.com>
2018-10-04 12:43:52 +0800

    Add following fuctions: 1. pull user image from registry server 2. using clair-scanner to scan user image 3. after scanning, remove the user image 4. return serverity number as exit code

--- windocker.sh
+++ windocker.sh
@@ -3,6 +3,36 @@
 ServerIP=$1
 ImageName=$2
 UserID=$3
+Threshold=$4
 ScanDate=`date +%Y%m%d-%H%M%S`
+JsonFilePath=`pwd`
+ScanResultFile=$JsonFilePath/$UserID-$ScanDate.json
 
-clair-scanner --ip $ServerIP -r $UserID-$ScanDate.json $ImageName
+## Functions Section
+GrepResult() {
+  local ResultFile=$1
+  local Threshold=$2
+  local Serverity=`grep $Threshold $ResultFile | wc -l`
+  return $Serverity
+}
+
+
+## Main Program Section
+
+# chekc number of parameter
+[ "$#" -lt 4 ] && echo "The number of parameter is less than 3.  Stop here." && exit 0
+
+# pull images from registry & scan image
+docker pull $ImageName && clair-scanner --ip $ServerIP -r $JsonFilePath/$UserID-$ScanDate.json $ImageName 1>/dev/null
+
+# check report & return serverity
+if [ -e $ScanResultFile ]; then
+    GrepResult $ScanResultFile $Threshold
+    Serverity=$?
+    docker rmi $ImageName
+    exit $Serverity
+else
+    echo "No Such Scan Report"
+fi
+
+
